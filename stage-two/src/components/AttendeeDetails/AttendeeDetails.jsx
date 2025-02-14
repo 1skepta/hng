c38e9db3b94abb80804bb8e807ab82810a8c0951
dankukennedy <dankukennedy@gmail.com>
2025-02-14 07:54:44 +0000

    all done

--- stage-two/src/components/AttendeeDetails/AttendeeDetails.jsx
+++ stage-two/src/components/AttendeeDetails/AttendeeDetails.jsx
@@ -14,7 +14,7 @@ function AttendeeDetails() {
           <span className={styles.light}>.</span>
           <span className={styles.dark}></span>
         </div>
-        <div className=" text-4xl text-green-400 text-wrap text-center">Remember i fixed this. The name is still Mr Pee !!!
+        <div className=" text-4xl text-green-400 text-wrap text-center">Skepa Remember i fixed this. The name is still Mr Pee !!!
            <p className="text-4xl text-teal-500 cursor-pointer hover:text-red-400">Hello Skepta </p>
         </div>
       </header>
