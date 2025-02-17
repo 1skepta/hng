3ac8b26d7ac94b509fb3430ff07a6cb658c4c340
1skepta <prospaul999@gmail.com>
2025-02-17 06:19:08 +0000

    working

--- stage-three/src/components/OutputDisplay.jsx
+++ stage-three/src/components/OutputDisplay.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function OutputDisplay() {
   return (
     <div>
-      <div className="text-center">
-        <p className="text-3xl font-extrabold">Type In Your Text!</p>
+      <div className="w-3/5 text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <p className="text-3xl font-extrabold mb-3">Type In Your Text!</p>
         <p>Let Me See How I Can Help</p>
       </div>
     </div>
