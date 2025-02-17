3ede8d7e87ee429aa48455ab9c1053a443f86192
1skepta <prospaul999@gmail.com>
2025-02-17 12:29:04 +0000

    workin g

--- stage-three/src/components/ActionButtons.jsx
+++ stage-three/src/components/ActionButtons.jsx
@@ -11,13 +11,13 @@ function ActionButtons() {
         <div className="p-1 px-2 bg-gray-100 rounded-xl">Summarize</div>
       </div>
       <div
-        className="p-3 rounded-lg"
+        className="p-5 rounded-2xl mt-1 z-10"
         style={{
           border: "1px solid #f0f0f0",
           boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.1",
         }}
       >
-        <ul>
+        <ul className="leading-9">
           <li>English - Detected</li>
           <li>Portuguese</li>
           <li>Spanish</li>
