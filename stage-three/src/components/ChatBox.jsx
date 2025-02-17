5e6bbd0fe1cc38aa575c16c7f1379b560e075506
1skepta <prospaul999@gmail.com>
2025-02-17 05:52:31 +0000

    working

--- stage-three/src/components/ChatBox.jsx
+++ stage-three/src/components/ChatBox.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Send } from "lucide-react";
 
 function ChatBox() {
-  return <div>ChatBox</div>;
+  return (
+    <div
+      className="justify-between flex p-3 rounded-2xl items-center fixed bottom-0 left-0 right-0 z-10 mb-5 mx-5"
+      style={{
+        border: "1px solid #f0f0f0",
+        boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.1",
+        transition: "bottom 0.3s ease-in-out",
+      }}
+    >
+      <div>
+        <input type="text" placeholder="Message SkeptaGPT" />
+      </div>
+      <button className="bg-black p-2 rounded-full">
+        <Send size={24} color="white" />
+      </button>
+    </div>
+  );
 }
 
 export default ChatBox;
--- stage-three/src/components/Landing.jsx
+++ stage-three/src/components/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import ChatBox from "./ChatBox";
 
 function Landing() {
   return (
     <div className="p-5">
       <Header />
+      <ChatBox />
     </div>
   );
 }
