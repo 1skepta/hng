8b5da0dd9a24c2664221ec7b6291fd5578e97324
1skepta <prospaul999@gmail.com>
2025-02-17 12:25:38 +0000

    working

--- stage-three/src/components/ActionButtons.jsx
+++ stage-three/src/components/ActionButtons.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { ChevronDown } from "lucide-react";
 
 function ActionButtons() {
   return (
-    <div className="flex pl-3 text-sm">
-      <div className="p-1 px-2 mr-2 bg-gray-100 rounded-xl">
-        English (Detected)
+    <div className="ml-3 text-sm">
+      <div className="flex">
+        <div className="p-1 px-2 mr-2 bg-gray-100 rounded-xl flex items-center">
+          English <ChevronDown />
+        </div>
+        <div className="p-1 px-2 bg-gray-100 rounded-xl">Summarize</div>
       </div>
-      <div className="p-1 px-2 bg-gray-100 rounded-xl">Summarize</div>
+      <div
+        className="p-3 rounded-lg"
+        style={{
+          border: "1px solid #f0f0f0",
+          boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.1",
+        }}
+      >
+        <ul>
+          <li>English - Detected</li>
+          <li>Portuguese</li>
+          <li>Spanish</li>
+          <li>Russian</li>
+          <li>Turkish</li>
+          <li>French</li>
+        </ul>
+      </div>
     </div>
   );
 }
